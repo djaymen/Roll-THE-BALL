a6cb299248e89d301a72589a09edce2a753de072
Djelid <63466137+djaymen@users.noreply.github.com>
2021-02-10 20:08:00 +0100

    update partie

--- Models/Partie.java
+++ Models/Partie.java
@@ -4,20 +4,14 @@ import java.io.Serializable;
 
 public class Partie implements Serializable {
 
-
-    Utilisateur utilisateur;
-    Niveau niveau;
-
+    private Utilisateur utilisateur;
+    private Niveau niveau;
 
     public Partie(Utilisateur utilisateur, Niveau niveau)
     {
         this.utilisateur = utilisateur;
         this.niveau = niveau;
     }
-
-
-
-
 
     public Utilisateur getUtilisateur() {
         return utilisateur;
